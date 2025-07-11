278bac5692b67c93067f39eedb6b20dd2dee139d
Kenneth Chen <kchen122804@gmail.com>
2025-07-11 00:13:35 -0400

    Updated

--- next.config.mjs
+++ next.config.mjs
@@ -4,9 +4,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  // If your repo is not at the root domain, set the basePath and assetPrefix:
-  // basePath: '/your-repo-name',
-  // assetPrefix: '/your-repo-name/',
-}
+  basePath: '/Scrapbook',
+  assetPrefix: '/Scrapbook/',
+};
 
-export default nextConfig; 
+export default nextConfig;
